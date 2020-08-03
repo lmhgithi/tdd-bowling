d5838a225db6a47a36711d8b81048bc969d9929e
lmhgithi <lmhfox@foxmail.com>
2020-08-03 15:36:49 +0800

    [liminghao]: implment get score of entire match when has spare before 10th frame

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,8 +1,34 @@
 public class BowlingGame {
+    static int maxFrameNum = 10;
     int score = 0;
+    int curFrameNum = 0, curFrameScore = 0, curFrameThrowTimes = 0;
+    int curAdd = 0, nextAdd = 0, next2Add = 0;
 
     public void Bowling(int hitDownNumber) {
+        if (curFrameNum > maxFrameNum) {
+            return;
+        }
+        curFrameThrowTimes++;
+        curAdd = nextAdd;
+        nextAdd = next2Add;
+        next2Add = 0;
+
+        if (curAdd != 0) {
+            this.score += hitDownNumber * curAdd;
+        }
+
         this.score += hitDownNumber;
+        curFrameScore += hitDownNumber;
+
+        if (curFrameThrowTimes == 2) {
+            if (curFrameScore == 10) {
+                nextAdd++;
+            }
+            curFrameNum++;
+            curFrameScore = 0;
+            curFrameThrowTimes = 0;
+        }
+
     }
 
     public int getScore() {
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -18,4 +18,19 @@ public class BowlingGameTest {
         }
         assertEquals(game.getScore(), 40);
     }
+
+    @Test
+    public void should_give_score_of_entire_match_when_has_spare_before_10th_frame(){
+        BowlingGame game = new BowlingGame();
+        game.Bowling(2);
+        game.Bowling(8);
+        for (int i = 0; i < 14; i++) {
+            game.Bowling(2);
+        }
+        game.Bowling(6);
+        game.Bowling(4);
+        game.Bowling(2);
+        game.Bowling(2);
+        assertEquals(game.getScore(), 56);
+    }
 }
